9e8051d973b45914cb85692a6cf908fb923fef3f
relizabethk <150353309+relizabethk@users.noreply.github.com>
2023-12-17 23:40:17 +0300

    Add files via upload

---
+++ lab3/infa.java
@@ -0,0 +1,10 @@
+public class infa {
+    private final String surname = "Кремез";
+    private final int group = 6;
+    private final String FirstName = "Елизавета";
+
+    @Override
+    public String toString() {
+        return "Фамилия Имя: " + surname + " " + FirstName + ", группа:" + group;
+    }
+}
